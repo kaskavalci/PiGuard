d04b3be588111920643bbea3660cc5910501a395
Halil Kaskavalci <comexx@gmail.com>
2018-12-28 19:12:27 +0100

    detect and log multiple faces

--- recognizer/recog.py
+++ recognizer/recog.py
@@ -29,30 +29,41 @@ face_encodings_pickle = "encodings.pickle"
 unknown = 'unknown'
 images_dir = 'images'
 
+
 class Recognizer():
     _args = None
     # Initialize some variables
     known_images_encoding = []
     _table = None
     _face_encodings = None
+    _recognized_faces = []
+    _recognized_names = {}
 
     def __init__(self, args):
         self._args = args
-        self._table = boto3.resource('dynamodb', region_name=AWS_REGION).Table(args.table)
+        self._table = boto3.resource(
+            'dynamodb', region_name=AWS_REGION).Table(args.table)
         unknown_dir = path.join(images_dir, unknown)
         if not path.exists(unknown_dir):
             makedirs(unknown_dir)
 
         # Load known pictures
         if not path.isfile(face_encodings_pickle):
-            self.download(path.join(AWS_FACE_ENCODINGS_BUCKET, face_encodings_pickle), face_encodings_pickle)
+            self.download(path.join(AWS_FACE_ENCODINGS_BUCKET,
+                                    face_encodings_pickle), face_encodings_pickle)
 
-        self._face_encodings = pickle.loads(open("encodings.pickle", "rb").read())
+        self._face_encodings = pickle.loads(
+            open("encodings.pickle", "rb").read())
         for name in self._face_encodings["names"]:
             image_dir = path.join(images_dir, name)
             if not path.exists(image_dir):
                 makedirs(image_dir)
                 print('created %s' % image_dir)
+
+        # Initialize as no face has been recognized
+        for i, name in enumerate(self._face_encodings["names"]):
+            self._recognized_names[name] = False
+            self._recognized_faces.insert(i, False)
 
         print('finished initialization of Recognizer')
 
@@ -84,7 +95,8 @@ class Recognizer():
         # Find all the faces and face encodings in the current frame of video
         encodings = face_recognition.face_encodings(small_frame)
 
-        recognized_face = unknown
+        recognized_faces = dict(self._recognized_names)
+
         for face_encoding in encodings:
             # See if the face is a match for the known face(s)
             match = face_recognition.compare_faces(
@@ -92,34 +104,38 @@ class Recognizer():
                 face_encoding, 0.6)
 
             for i, m in enumerate(match):
-                if m:
-                    recognized_face = self._face_encodings["names"][i]
-            if recognized_face != unknown:
-                break
-            # face_names.append(name)
+                name = self._face_encodings["names"][i]
+                if m and not recognized_faces[name]:
+                    recognized_faces[name] = True
 
-        # move to recognized or unknown folder
-        image_path = path.join(images_dir, recognized_face, image_name)
-        rename(path.join(images_dir, image_name), image_path)
         # end of algorithm. no need to track uploading time
         elapsed = str(time.time() - start)
+        # # move to recognized or unknown folder
+        # image_path = path.join(images_dir, recognized_face, image_name)
+        # rename(path.join(images_dir, image_name), image_path)
 
         # upload only if we cannot recognize the face
-        if recognized_face == unknown and self._args.upload:
-            thread = Thread(target=self.upload, args=(image_name, image_path, frame))
+        imgdir = path.join(images_dir, image_name)
+        if self._args.upload:
+            thread = Thread(target=self.upload, args=(
+                image_name, imgdir, frame))
             thread.start()
 
         print ('elapsed time %s' % elapsed)
 
-        table_put_response = self._table.put_item(
-            Item={
-                'filename': image_name,
-                'result': recognized_face,
-                'created': str(datetime.datetime.utcnow()),
-                'duration': elapsed
-            }
-        )
-        return recognized_face
+        db_row = {
+            'filename': image_name,
+            'created': str(datetime.datetime.utcnow()),
+            'duration': elapsed
+        }
+
+        for name, result in recognized_faces.iteritems():
+            db_row["recognized_" + name] = str(result)
+
+        self._table.put_item(Item=db_row)
+
+        return recognized_faces
+
 
 class PUTHandler(BaseHTTPRequestHandler):
     recognizer = None
@@ -131,7 +147,7 @@ class PUTHandler(BaseHTTPRequestHandler):
             self.send_response(500)
             return
         d = self.rfile.read(int(self.headers['Content-Length']))
-        self.send_response(204) # Return early response
+        self.send_response(204)  # Return early response
         self.end_headers()
 
         image_name = str(uuid.uuid4()) + ".jpg"
@@ -160,9 +176,14 @@ def run_on(addr, port, args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Face Tracker')
-    parser.add_argument('--upload', action='store_true', dest='upload', default=False)
-    parser.add_argument('--s3', action='store', dest='s3_bucket', default='unrecognized-faces-1')
-    parser.add_argument('--dynamo-table', action='store', dest='table', default='local-stats')
+    parser.add_argument('--upload', action='store_true',
+                        dest='upload', default=False)
+    parser.add_argument('--host', action='store',
+                        dest='host', default=socket.gethostname())
+    parser.add_argument('--s3', action='store',
+                        dest='s3_bucket', default='unrecognized-faces-1')
+    parser.add_argument('--dynamo-table', action='store',
+                        dest='table', default='local-stats')
 
     args = parser.parse_args()
-    run_on(socket.gethostname(), 8080, args)
+    run_on(args.host, 8080, args)
